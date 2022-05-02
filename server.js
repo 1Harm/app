02897cb91a3e2eea3e0d46515d75bf25f6d71e8a
1Harm <ghost9029@mail.ru>
2022-05-02 14:26:00 +0600

    Yeldar

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ app.use("/fifth_pizza",require("./routes/fifth_pizza"));
 app.use("/sixth_pizza",require("./routes/sixth_pizza"));
 app.use("/sale",require("./routes/sale"));
 app.use("/reg",require("./routes/reg"));
-
-
-
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`)
 );
@@ -38,20 +35,52 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
+module.exports = mongoose => {
+    const Tutorial = mongoose.model(
+        "tutorial",
+        mongoose.data(
+            { email: {
+        type: String,
+            required: true,
+            unique: true
+    },
+    name: {
+        type: String,
+    default: ''
+    },
+    pass: {
+        type: String,
+    default: ''
+    },
+    phone: String, }
+        )
+    );
+    return Tutorial;
+};
 app.post('/sign_up', function(req,res){
     let name = req.body.name;
     let email = req.body.email;
     let pass = req.body.password;
     let phone = req.body.phone;
 
-    let data = {
-        "name": name,
-        "email": email,
-        "password": pass,
-        "phone": phone
-    };
-    db.collection('Harm').insertOne(data,function(err, collection){
+    let Data =mongoose.data({
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        name: {
+            type: String,
+            default: ''
+        },
+        pass: {
+            type: String,
+            default: ''
+        },
+        phone: String,
+    });
+
+    db.collection('Harm').insertOne(Data,function(err, collection){
         if (err) throw err;
         console.log("Record inserted Successfully");
 
@@ -59,8 +88,6 @@ app.post('/sign_up', function(req,res){
 
     return res.redirect('Register.ejs');
 })
-
-
 app.get('/',function(req,res){
     res.set({
         'Access-control-Allow-Origin': '*'
@@ -68,5 +95,119 @@ app.get('/',function(req,res){
     return res.redirect('Register.ejs');
 }).listen(3000)
 
-
 console.log("server listening at port 7777");
+const db = require("../models");
+const Tutorial = db.tutorials;
+const UserModel = require('../model/user')
+// Create and Save a new user
+exports.create = async (req, res) => {
+    if (!req.body.email && !req.body.firstName && !req.body.lastName && !req.body.phone) {
+        res.status(400).send({ message: "Content can not be empty!" });
+    }
+
+    const user = new UserModel({
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phone: req.body.phone
+    });
+
+    await user.save().then(data => {
+        res.send({
+            message:"User created successfully!!",
+            user:data
+        });
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating user"
+        });
+    });
+};
+// Retrieve all users from the database.
+exports.findAll = async (req, res) => {
+    try {
+        const user = await UserModel.find();
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+};
+// Find a single User with an id
+exports.findOne = async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.params.id);
+        res.status(200).json(user);
+    } catch(error) {
+        res.status(404).json({ message: error.message});
+    }
+};
+// Update a user by the id in the request
+exports.update = async (req, res) => {
+    if(!req.body) {
+        res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+
+    const id = req.params.id;
+
+    await UserModel.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then(data => {
+        if (!data) {
+            res.status(404).send({
+                message: `User not found.`
+            });
+        }else{
+            res.send({ message: "User updated successfully." })
+        }
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+};
+// Delete a user with the specified id in the request
+exports.destroy = async (req, res) => {
+    await UserModel.findByIdAndRemove(req.params.id).then(data => {
+        if (!data) {
+            res.status(404).send({
+                message: `User not found.`
+            });
+        } else {
+            res.send({
+                message: "User deleted successfully!"
+            });
+        }
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
+    });
+};
+// Create and Save a new Tutorial
+exports.create = (req, res) => {
+
+};
+// Retrieve all Tutorials from the database.
+exports.findAll = (req, res) => {
+
+};
+// Find a single Tutorial with an id
+exports.findOne = (req, res) => {
+
+};
+// Update a Tutorial by the id in the request
+exports.update = (req, res) => {
+
+};
+// Delete a Tutorial with the specified id in the request
+exports.delete = (req, res) => {
+
+};
+// Delete all Tutorials from the database.
+exports.deleteAll = (req, res) => {
+
+};
+// Find all published Tutorials
+exports.findAllPublished = (req, res) => {
+
+};
